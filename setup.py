299162c4ad61c1c74e446fb9c681699ba836d52d
Ilan Schnell <ischnell@enthought.com>
2011-04-20 18:41:24 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+version = '0.1.0'
+
+
+setup(
+    name = 'etsproxy',
+    version = version,
+    author = 'Enthought, Inc.',
+    author_email = 'info@enthought.com',
+    download_url = ('http://www.enthought.com/repo/ets/etsproxy-%s.tar.gz' %
+                    version),
+    license = 'BSD',
+    maintainer = 'ETS Developers',
+    maintainer_email = 'enthought-dev@enthought.com',
+)
