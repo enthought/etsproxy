e25a1c55d192c0ad53e4fb76840759b9b630bdf8
Ilan Schnell <ischnell@enthought.com>
2011-04-20 18:46:17 -0500

    add missing packages keyword argument

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ version = '0.1.0'
 setup(
     name = 'etsproxy',
     version = version,
+    packages = find_packages(),
     author = 'Enthought, Inc.',
     author_email = 'info@enthought.com',
     download_url = ('http://www.enthought.com/repo/ets/etsproxy-%s.tar.gz' %
