2010868cadf7c72a9dc96949b697e6d9b639f60b
epatters <epatters@enthought.com>
2011-06-01 15:30:07 -0500

    Better proxy for traits.qt: no code duplication.

--- enthought/qt/__init__.py
+++ enthought/qt/__init__.py
@@ -9,18 +9,4 @@
 # Description: Qt API selector. Can be used to switch between pyQt and PySide
 #------------------------------------------------------------------------------
 
-import os
-
-from enthought.etsconfig.api import ETSConfig
-
-qt_api = os.environ.get('QT_API', 'pyqt')
-
-if ETSConfig.toolkit == 'qt4':
-
-    if qt_api == 'pyqt':
-        import sip
-        sip.setapi('QString', 2)
-        sip.setapi('QVariant', 2)
-
-    else:
-        print "---- using PySide ----"
+from traits.qt import qt_api
