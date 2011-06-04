95d16b818d42559229aa383e6cc1be76495abcf2
Robert Kern <robert.kern@gmail.com>
2011-06-04 17:47:30 -0500

    ENH: Add the removed modules from traits.util with deprecation warnings.

--- enthought/util/updates/update_info.py
+++ enthought/util/updates/update_info.py
@@ -1,2 +1,33 @@
-# proxy module
-from traits.util.updates.update_info import *
+import warnings
+warnings.warn("Module is deprecated.", DeprecationWarning)
+
+
+
+
+
+class UpdateInfo:
+    """ Encapsulates the information about the available update or
+    updates.  An update can consist of multiple versions, with each
+    version containing its own information and download URL.
+    """
+
+    # A list of VersionInfo objects
+    updates = None
+
+    #========================================================================
+    # Constructors
+    #========================================================================
+
+    @classmethod
+    def from_uri(cls, uri):
+        """ Returns a new UpdateInfo, with a populated list of VersionInfo
+        objects
+        """
+        raise NotImplementedError
+
+
+    def __init__(self, updates=None):
+        if updates:
+            self.updates = updates
+        return
+
