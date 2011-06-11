e3db7477c21c4d7cea12b3000146567626712168
Ilan Schnell <ischnell@enthought.com>
2011-06-11 00:15:14 -0500

    remove naming.traits proxy

--- enthought/naming/traits/api.py
+++
@@ -1,3 +0,0 @@
-# proxy module
-from __future__ import absolute_import
-from apptools.naming.trait_defs.api import *
--- enthought/naming/traits/naming_traits.py
+++
@@ -1,3 +0,0 @@
-# proxy module
-from __future__ import absolute_import
-from apptools.naming.traits.naming_traits import *
